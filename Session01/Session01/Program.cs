7ac43c69821590ef18a833939c3d8452cc4fd550
Adelina <adelinalica2@gmail.com>
2017-11-20 22:35:36 +0200

    First Homework

---
+++ Session01/Session01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session01
+{
+    public struct Animal
+    {
+        public String nume;
+        public String sunet;
+
+
+        public void getValues(string n, string s)
+        {
+            nume = n;
+            sunet = s;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            String[] a = { "dog", " cat", " frog" };
+            String[] b = { "ham-ham", "miau-miau", "oac-oac" };
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                animal.getValues(a[i], b[i]);
+                Console.WriteLine(animal.nume + " makes " + animal.sunet);
+            }
+        }
+    }
+}
