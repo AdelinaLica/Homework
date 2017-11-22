70d3c5bb0b45076e2f8083da4a2f3b1564a7eb4d
Cosmin Barbus <cosmin.barbus@gmail.com>
2017-11-22 12:16:12 +0200

    Update Program.cs

--- Session01/Session01/Program.cs
+++ Session01/Session01/Program.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 
 namespace Session01
 {
+    // Why struct and not class?
     public struct Animal
     {
         public String nume;
         public String sunet;
 
-
+// TODO: 
+// 1. the method name does not reflect what it realy does(set is more appropiate)
+// 2. the name of varibles should not be abreviated
+// 3. Why do you need a getter/setter if the "nume", "sunet" are public
+// 4. Be consitent using the english or romanian (english recommended)
+// 5. For method naming conventions please check the following articles:
+        // https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines/naming-guidelines
+        // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/inside-a-program/coding-conventions#naming-conventions
         public void getValues(string n, string s)
         {
             nume = n;
@@ -22,10 +30,15 @@ namespace Session01
     {
         static void Main(string[] args)
         {
+            // Even though the result is as expected, OOP is not used (inheritance is not used, encapsulation)
             Animal animal = new Animal();
+            
+            // break the encapsulation of the Dog or Cat objects; hint: try to create a class for each animal.
             String[] a = { "dog", " cat", " frog" };
             String[] b = { "ham-ham", "miau-miau", "oac-oac" };
 
+            // what happens if the a has more items then b
+            // please never ever use just letters for variable names (only in loops, e.g. "i" in for)
             for (int i = 0; i < a.Length; i++)
             {
                 animal.getValues(a[i], b[i]);
